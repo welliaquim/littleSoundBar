e1eaf43dd6e5111eb0d761216026c51f4e0876f3
Welliaquim Bartolomeo de Souza <welliaquimquim@gmail.com>
2024-04-04 22:40:39 -0400

    Aprimorar código presente no main.js

--- main.js
+++ main.js
@@ -1,37 +1,16 @@
-function soundPom() {
-  document.querySelector('#som_tecla_pom').play();
-}
-function soundClap() {
-  document.querySelector('#som_tecla_clap').play();
-}
-function soundTim() {
-  document.querySelector('#som_tecla_tim').play();
-}
-function soundPuff() {
-  document.querySelector('#som_tecla_puff').play();
-}
-function soundSplash() {
-  document.querySelector('#som_tecla_splash').play();
-}
-function soundToim() {
-  document.querySelector('#som_tecla_toim').play();
-}
-function soundPsh() {
-  document.querySelector('#som_tecla_psh').play();
-}
-function soundTic() {
-  document.querySelector('#som_tecla_tic').play();
-}
-function soundTom() {
-  document.querySelector('#som_tecla_tom').play();
+function audioElement(idAudio) {
+  document.querySelector(idAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = soundPom;
-document.querySelector('.tecla_clap').onclick = soundClap;
-document.querySelector('.tecla_tim').onclick = soundTim;
-document.querySelector('.tecla_puff').onclick = soundPuff;
-document.querySelector('.tecla_splash').onclick = soundSplash;
-document.querySelector('.tecla_toim').onclick = soundToim;
-document.querySelector('.tecla_psh').onclick = soundPsh;
-document.querySelector('.tecla_tic').onclick = soundTic;
-document.querySelector('.tecla_tom').onclick = soundTom;
+const arrTeclas = document.querySelectorAll('.tecla');
+
+let contagemArrTeclas = 0;
+
+while (contagemArrTeclas < arrTeclas.length) {
+  const tecla = arrTeclas[contagemArrTeclas];
+  const instrumentoSom = tecla.classList[1];
+  const idInstrumentoSom = `#som_${instrumentoSom}`;
+
+  tecla.onclick = () => audioElement(idInstrumentoSom);
+  contagemArrTeclas++;
+}
